da6b178f99c0770264ec69adb010e6a66e5b5081
selma <belhadjselma1@gmail.com>
2022-08-09 23:16:35 +0100

    Delete the things controller & update the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root 'static#index'
   
   namespace :v1, defaults: { format: :json } do
-    get 'things', to: 'things#index'
     get 'greetings', to: 'greetings#index'
   end
 
